852bd6ea84f0f3d6178f2c7da805713e1ee36c9a
maeda-seina <ayrton19601027@gmail.com>
2021-12-14 12:43:33 +0900

    無名関数を削除し、コンポーネントのdataを関数に変更

--- js/main.js
+++ js/main.js
@@ -1,68 +1,66 @@
-(function () {
-
-  'use strict';
-
-  const vm = new Vue({
-    el: '#app',
-    data: {
+const appdata = {
+  data() {
+    return {
       newItem: '',
       editIndex: -1,
       todos: []
-    },
-    watch: {
-      todos: {
-        handler: function () {
-          localStorage.setItem('todos', JSON.stringify(this.todos))
-        },
-        deep: true
-      }
-    },
-    mounted: function () {
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods: {
-      setItems: function () {
-        if (this.editIndex === -1) {
-          const item = {
-            title: this.newItem,
-            isDone: false
-          }
-          this.todos.push(item);
-          this.newItem = '';
-        } else {
-          this.todos.splice(this.editIndex, 1, this.newItem)
-        }
-        this.cancel()
-      },
-      deleteItem: function (index) {
-        if (confirm('are you sure?')) {
-          this.todos.splice(index, 1)
-        }
-      },
-      clearDeleteTask: function () {
-        if (!confirm('delete finished?')){
-          return;
-        }
-        this.todos = this.remaining
-      },
-      edit: function (index) {
-        this.editIndex = index
-        this.newItem = this.todos[index]
-      },
-      cancel: function () {
-        this.newItem = ""
-        this.editIndex = -1
-      }
-    },
-    computed: {
-      remaining: function () {
-        return this.todos.filter(function (todo) {
-          return !todo.isDone
-        })
-      },
-      changeButtonText: function () {
-        return this.editIndex === -1 ? 'Add' : 'Update'
-      }
     }
-  })
-})();
+  },
+  watch: {
+    todos: {
+      handler: function () {
+        localStorage.setItem('todos', JSON.stringify(this.todos))
+      },
+      deep: true
+    }
+  },
+  mounted: function () {
+    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+  },
+  methods: {
+    setItems: function () {
+      if (this.editIndex === -1) {
+        const item = {
+          title: this.newItem,
+          isDone: false
+        }
+        this.todos.push(item);
+        this.newItem = '';
+      } else {
+        this.todos.splice(this.editIndex, 1, this.newItem)
+      }
+      this.cancel()
+    },
+    deleteItem: function (index) {
+      if (confirm('are you sure?')) {
+        this.todos.splice(index, 1)
+      }
+    },
+    clearDeleteTask: function () {
+      if (!confirm('delete finished?')) {
+        return;
+      }
+      this.todos = this.remaining
+    },
+    edit: function (index) {
+      this.editIndex = index
+      this.newItem = this.todos[index]
+    },
+    cancel: function () {
+      this.newItem = ""
+      this.editIndex = -1
+    }
+  },
+  computed: {
+    remaining: function () {
+      return this.todos.filter(function (todo) {
+        return !todo.isDone
+      })
+    },
+    changeButtonText: function () {
+      return this.editIndex === -1 ? 'Add' : 'Update'
+    }
+  }
+}
+let app = Vue.createApp(appdata)
+app.mount('#app')
